454ace31edbdc2050e50046360fa3cc6204c3427
Lily Cov <lily@bettersites.com>
2023-08-10 18:32:32 -0700

    finally done with spies

--- src/_01_Spies_On_A_Train/SpiesOnATrain.java
+++ src/_01_Spies_On_A_Train/SpiesOnATrain.java
@@ -25,6 +25,45 @@ public class SpiesOnATrain {
     	String spy = null;
     	boolean found = false;
     	Node<TrainCar> passenger = train.getHead();
+    	HashMap<String, Integer> map = new HashMap<>();
+    	
+    	while(passenger !=null) {
+    		map.put(passenger.getValue().toString(), 0);
+    		passenger = passenger.getNext();
+    	}
+    	
+    	passenger = train.getHead();
+    	while(passenger != null) {
+    		String answer = passenger.getValue().questionPassenger();
+    		answer = answer.substring(answer.indexOf("I saw "));
+    		
+    		for(String c: clues){
+    			if(answer.contains(c)) {
+    				for(String d: map.keySet()){
+    					if(answer.contains(d)) {
+    						int matching = map.get(d) +1;
+    						if(matching == clues.length) {
+    							return d;
+    						}
+    						map.put(d, matching);
+    					}
+    				}
+    			}
+    		}
+    		
+    		
+    		
+    		passenger = passenger.getNext();
+    		
+    	}
+    	
+    	return null;
+    	
+    /*	for(int i = 0; i<train.size(); i++) {
+    	map.put(train.toString(), 0);
+    	//System.out.println(train.to);
+    	}
+    	
     	while(passenger != null && !found) {
     		String info = passenger.getValue().questionPassenger();
     		TrainCar trainCar = new TrainCar(passenger.getValue().toString(), info);
@@ -38,24 +77,29 @@ public class SpiesOnATrain {
     		
     		System.out.println(name1 + ", " + evidence1);
     		
+    		
     		for(int k = 0; k<clues.length; k++) {
     				//System.out.println(clues[k].toString());
-    				/*if(info.) {
-    					
-    				}*/
+    			int matchingClues = 0;
+    				if(evidence1.equalsIgnoreCase(clues[k])) {
+    					matchingClues++;
+    					map.replace(name1, matchingClues);
+    					if(matchingClues>=3) {
+    						found=true;
+    					}
+    				}
     				//analyze info to determine if I can eliminate suspect (2nd name in  string info)
-    				//compare passenger witness testimony to clues
-    		
-    		
-    		
     		}
+    		spy = name1;
+    		System.out.println(name1);
     		passenger = passenger.getNext();
     		}
     		
     	
-    	spy = passenger.getValue().toString();
+    	//spy = passenger.getValue().toString();
         return spy;
 
-    }
+    } */
 
+}
 }
